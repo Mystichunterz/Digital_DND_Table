9cf42694022c00214aead91b6cd122f825289df2
Mystichunterz <131076575+Mystichunterz@users.noreply.github.com>
2026-05-03 10:10:28 +0800

    feat(layout): Wrap V2 shell in ConditionsProvider

--- src/v2/layout/V2Layout.jsx
+++ src/v2/layout/V2Layout.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import V2LeftPanel from "./V2LeftPanel";
 import V2TabBar from "./V2TabBar";
+import { ConditionsProvider } from "../state/ConditionsContext";
 import "../styles/v2-layout.scss";
 
 const V2Layout = () => {
@@ -29,21 +30,23 @@ const V2Layout = () => {
   }, []);
 
   return (
-    <div className="v2-shell">
-      <aside className="v2-left-panel">
-        <V2LeftPanel />
-      </aside>
+    <ConditionsProvider>
+      <div className="v2-shell">
+        <aside className="v2-left-panel">
+          <V2LeftPanel />
+        </aside>
 
-      <main className="v2-right-panel">
-        <div className="v2-right-pane">
-          <V2TabBar />
+        <main className="v2-right-panel">
+          <div className="v2-right-pane">
+            <V2TabBar />
 
-          <section className="v2-right-surface">
-            <Outlet />
-          </section>
-        </div>
-      </main>
-    </div>
+            <section className="v2-right-surface">
+              <Outlet />
+            </section>
+          </div>
+        </main>
+      </div>
+    </ConditionsProvider>
   );
 };
 
